f9655b2d785d8f8891a9136c638c5b48dc32c7fc
Alexander Frosell <sachafrosell@gmail.com>
2018-10-09 18:18:12 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person 
+
+  def name 
+    @name 
+  end
+  
+  def name=(person_name)
+    @name = name 
+  end 
+  
+  def job 
+    @job
+  end 
+  
+  def job=(person_job)
+    @job = job
+  end 
+  
+  
+end 
